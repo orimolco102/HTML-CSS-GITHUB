b414617ccda1e0bcfd837bef44d51c0dc4d414d6
orimolco102 <orimolco102@gmail.com>
2026-05-03 21:51:40 +0300

    end project

--- projects/RPS/script.js
+++ projects/RPS/script.js
@@ -1,34 +1,21 @@
-const score = {
+let score = JSON.parse(localStorage.getItem("score")) || {
   Wins: 0,
   Losses: 0,
   Ties: 0,
 };
-
-function PickComputerMove() {
-  const randomNumber = Math.random();
-  let computerMove = "";
-
-  if (randomNumber >= 0 && randomNumber < 1 / 3) {
-    computerMove = "Rock";
-  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
-    computerMove = "Paper";
-  } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
-    computerMove = "Scissors";
-  }
-
-  return computerMove;
-}
 
 function res(ButtonID) {
   const computerMove = PickComputerMove();
   let result = "";
   const PlayerChoice = ButtonID;
 
-  console.log(PlayerChoice);
-  console.log(computerMove);
-  console.log(PlayerChoice == computerMove);
-
-  if (PlayerChoice === computerMove) {
+  if (PlayerChoice === "clear") {
+    score = {
+      Wins: 0,
+      Losses: 0,
+      Ties: 0,
+    };
+  } else if (PlayerChoice === computerMove) {
     result = "Tie";
     score.Ties += 1;
   } else if (
@@ -38,14 +25,37 @@ function res(ButtonID) {
   ) {
     result = "You Win";
     score.Wins += 1;
-  } else  {
-    result = "You Lose"
+  } else {
+    result = "You Lose";
     score.Losses += 1;
   }
 
-  localStorage.setItem("score",JSON.stringify(score));
+  localStorage.setItem("score", JSON.stringify(score));
 
-  alert(`Computer picked: ${computerMove} result: ${result}
-    Wins: ${score.Wins}, Losses: ${score.Losses}, Ties: ${score.Ties}
-    `);
+  document.querySelector(".result").innerHTML = result;
+  document.querySelector(".picks").innerHTML =
+    `Computer picked: ${computerMove}. You have picked: ${PlayerChoice}`;
+
+  UpdateScore();
+
+  function UpdateScore() {
+    document.querySelector(".score").innerHTML =
+      `score: Wins: ${score.Wins}, Losses: ${score.Losses}, Ties: ${score.Ties}`;
+  }
+
+
+  function PickComputerMove() {
+    const randomNumber = Math.random();
+    let computerMove = "";
+
+    if (randomNumber >= 0 && randomNumber < 1 / 3) {
+      computerMove = "Rock";
+    } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+      computerMove = "Paper";
+    } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+      computerMove = "Scissors";
+    }
+
+    return computerMove;
+  }
 }
