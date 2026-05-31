be564af47ce26495719aa8b5fb4fff9eea059393
Ori <orimolco102@gmail.com>
2026-05-31 17:55:52 +0300

    cont

--- HTML-projects/user-table/app.js
+++ HTML-projects/user-table/app.js
@@ -1,6 +1,11 @@
-const express = require('express')
+const express = require('express');
 
 const app = express();
+
+app.use(express.json());
+app.get("/", (req, res) =>{
+    res.send('welcome');
+});
 
 const PORT = 3000;
 app.listen(PORT, ()=> {
